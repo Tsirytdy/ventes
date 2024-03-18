26a3c5bde54d549bdf25b5559467bb37d2e3ccf2
Tsirytdy <vonyvony59@gmail.com>
2024-03-18 14:16:32 +0100

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,8 +19,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products",
+            ? `https://ventes-api.onrender.com/api/products?category=${cat}`
+            : "https://ventes-api.onrender.com/api/products",
         );
         setProducts(res.data);
       } catch (err) {}
